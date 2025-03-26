fe43cfccc9fdcc166c44f0e97b1b3b0deda7bd13
harrismhurley <harrismhurley@icloud.com>
2025-03-26 02:08:28 -0500

    basic bot

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # bot
+
+add your own .env with a jamba API key
---
+++ script.py
@@ -0,0 +1,56 @@
+# %%
+import os
+from dotenv import load_dotenv
+from ai21 import AI21Client
+from ai21.models.chat.chat_message import AssistantMessage, SystemMessage, UserMessage
+
+load_dotenv()
+
+AI21_KEY = os.getenv('AI21_KEY')
+
+# %%
+client = AI21Client(
+    api_key = AI21_KEY
+)
+
+system = "You're a bot assistant"
+
+messages = [
+    SystemMessage(content=system, role="system"),
+    UserMessage(content="Hello, how can you help me?", role="user")
+]
+
+def chat(user_input):
+    messages.append(UserMessage(content=user_input, role="user"))
+
+    chat_response = client.chat.completions.create(
+        system=system,
+        messages=messages,
+        model="jamba-mini",
+        max_tokens=100,
+        temperature=0.7,
+        top_p=1.0,
+        stop=["\n"],
+        stream=True
+    )
+
+    full_response = []
+
+    for chunk in chat_response:
+        content = chunk.choices[0].delta.content
+        if content is not None:
+            full_response.append(content)
+
+    
+    assistant_response = "".join(full_response)
+    messages.append(AssistantMessage(content=assistant_response, role="assistant"))
+    
+    return assistant_response
+# %%
+while True:
+    user_input = input('input: ')
+    if user_input.lower() == 'exit':
+        break
+    bot_response = chat(user_input)
+    print(f"\nBot: {bot_response}\n")
+# %%
